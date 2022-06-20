e47efaec3e3d453b1ccbd709f751b2a2fac18115
Tazaev <tonymurtazaev@gmail.com>
2022-06-20 19:11:17 -0400

    added employee test

---
+++ tests/Employee.test.js
@@ -0,0 +1,34 @@
+const Employee = require("../lib/Employee.js");
+
+test("creates a new employee object", () => {
+  const employee = new Employee("Benjamin", 07, "Benjamin@yahoo.com");
+
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+});
+
+test("gets the employee name", () => {
+  const employee = new Employee("Benjamin", 07, "Benjamin@yahoo.com");
+
+  expect(employee.getName()).toBe("Benjamin");
+});
+
+test("gets employee id", () => {
+  const employee = new Employee("Benjamin", 07, "Benjamin@yahoo.com");
+
+  expect(employee.getId()).toBe(90);
+});
+
+test("gets employee email", () => {
+  const employee = new Employee("Benjamin", 07, "Benjamin@yahoo.com");
+
+  expect(employee.getEmail()).toBe("Benjamin@yahoo.com");
+});
+
+test("gets employee role", () => {
+  const employee = new Employee("Benjamin", 07, "Benjamin@yahoo.com");
+
+  expect(employee.getRole()).toBe("Employee");
+  console.log(employee);
+});
