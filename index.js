f1d4cb277ba34b7a0dba65c70f6c13dbc99f5b14
Tazaev <tonymurtazaev@gmail.com>
2022-06-20 20:59:20 -0400

    added to index.js file to call on generate

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const Employee = require("./Employee.js").default;
+const Generate = require("./lib/Generate.js");
 
-class Manager extends Employee {
-  constructor(name, id, email, officeNumber) {
-    super(name, id, email);
-    this.officeNumber = officeNumber;
-    this.role = "Manager";
-  }
-
-  getOfficeNumber() {
-    return this.officeNumber;
-  }
-}
-
-module.exports = Manager;
+new Generate().promptManager();
